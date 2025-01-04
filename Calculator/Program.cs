5e3a78f60cd33f4c711b736e999781c0c9954147
chaleksa <fldmouse@gmail.com>
2025-01-04 14:30:14 +0100

    Add sum calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        double myNumber1 = 0;
+        double myNumber2 = 0;
+
+        Console.WriteLine("Enter a number: ");
+        myNumber1 = ParseInput();
+
+        Console.WriteLine("Enter a second number: ");
+        myNumber2 = ParseInput();
+
+        Console.WriteLine($"{myNumber1} + {myNumber2} = {myNumber1 + myNumber2}");
+
+    }
+    public static double ParseInput()
+    {
+        double result = 0;
+        bool isParsed = double.TryParse(Console.ReadLine(), out result);
+
+        while (!isParsed)
+        {
+            Console.WriteLine("You entered invalid value. Enter a number!");
+            isParsed = double.TryParse(Console.ReadLine(), CultureInfo.CurrentCulture, out result);
+        }
+
+        return result;
+    }
+}
+
+
