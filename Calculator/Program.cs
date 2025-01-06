6ce33bc3c81743a032fa7de9fc947f36e270e8d4
chaleksa <fldmouse@gmail.com>
2025-01-06 10:22:47 +0100

    Rename method

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,19 +5,19 @@ class Program
 {
     static void Main()
     {
-        double myNumber1 = 0;
-        double myNumber2 = 0;
+        double myNumber1;
+        double myNumber2;
 
         Console.WriteLine("Enter a number: ");
-        myNumber1 = ParseInput();
+        myNumber1 = ReadAndParseInput();
 
         Console.WriteLine("Enter a second number: ");
-        myNumber2 = ParseInput();
+        myNumber2 = ReadAndParseInput();
 
         Console.WriteLine($"{myNumber1} + {myNumber2} = {myNumber1 + myNumber2}");
 
     }
-    public static double ParseInput()
+    public static double ReadAndParseInput()
     {
         double result = 0;
         bool isParsed = double.TryParse(Console.ReadLine(), out result);
